4d97694a5d4e580eea9f8bf86b63914c307dc218
tarasii <tarasii@gmail.com>
2019-10-02 01:24:23 +0300

    article page

--- pytable.py
+++ pytable.py
@@ -10,7 +10,7 @@ cgitb.enable()
 #example
 if __name__ == "__main__":
 
-  print(mh.Html("MyHtml mysql tabel list",
+  print(mh.Page("MyHtml mysql tabel list",
     mh.JavaScriptInc("js/jquery-1.8.2.js"),
     mh.JavaScript("console.log('start');"),
     mh.Header("Main:"),
